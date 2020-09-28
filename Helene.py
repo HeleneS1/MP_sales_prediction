37992ac86943d66c399c49e6037d5ad663bbf463
Helene Stabell <helenestabell1@gmail.com>
2020-09-28 16:04:54 +0200

    Main with problematic date(tensor)

--- Helene.py
+++ Helene.py
@@ -19,10 +19,13 @@ df_original.columns
 
 df = df_original[['date', 'date_block_num', 'shop_id', 'item_id', 'item_price', 'item_cnt_day']]
 df['Total_Sales_day'] = df['item_price'] * df['item_cnt_day']
-del df['item_cnt_day'], df['item_price']
 
 df_one_month = df.loc[df['date_block_num']== 0, :]
 df_grouped_shop = df_one_month[['shop_id', 'date', 'Total_Sales_day']].groupby(by=['shop_id', 'date']).sum().reset_index()
 
+#%%
+
+X_one_month = np.c_[df_grouped_shop[['shop_id', 'date']]]
+y_one_month = np.c_[df_grouped_shop['Total_Sales_day']]
 
 
