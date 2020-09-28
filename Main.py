67816091eb07f69d0189e38d6521f44c168f0e1f
malinhl <malinhl91@gmail.com>
2020-09-28 13:48:12 +0200

    'TotalSales-kolonne'

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
 """ This is the file where we connect all the dots """
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data= pd.read_csv('sales_train.csv')
+
+#Lager kopi av datasettet
+df_draft= data.copy()
+
+
+# Legger til ny kolonne:
+df_draft['Total_Sales_day']=df_draft['item_price']* df_draft['item_cnt_day']
+
+
+# Sjekker fordelingen:
+
+df_draft.hist()
