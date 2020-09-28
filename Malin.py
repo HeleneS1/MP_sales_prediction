8dd596297c7a7c9627eb2d3cfa448be5fb8997a2
malinhl <malinhl91@gmail.com>
2020-09-28 19:54:47 +0200

    Added date

--- Malin.py
+++ Malin.py
@@ -39,3 +39,35 @@ ohe_shop= pd.DataFrame(np_onehot,columns=onehot.categories_)
 
 # Legger sammen med OG-dataframe
 df_concat= pd.concat([df_draft, ohe_shop],axis=1)
+
+
+#%%
+# Fikser dato
+# OHE Dato
+import datetime as dt
+
+# Gjør om date til datetime-format:
+df_concat['date']= pd.to_datetime(df_concat['date'],format= '%d.%m.%Y')
+
+
+# Legger til dag, måned, år, kvartal:
+df_concat['month'] = df_concat['date'].dt.month
+
+df_concat['year'] = df_concat['date'].dt.year
+
+df_concat['day'] = df_concat['date'].dt.day
+
+df_concat['quarter'] = df_concat['date'].dt.quarter
+
+
+# Legger til day of week(numerisk) og tekst. 
+#NB! Mandag=0, søndag=6
+df_concat['dayofweek'] = df_concat['date'].dt.dayofweek # Numerisk
+
+df_concat['dayofweek_text'] = df_concat['date'].dt.day_name() #Gir i tekst
+
+
+# Sjekker om det er helg eller ikke:
+# np.where()= hvis sant, gjør x, ellers y
+df_concat['is_weekend']= np.where(df_concat['dayofweek_text'].isin(['Sunday','Saturday']),1,0)
+
